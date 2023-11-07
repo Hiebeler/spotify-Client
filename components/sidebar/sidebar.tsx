c8a8d4e44d108649e1718bb5e0ae205cc8296ce0
Hiebeler <emanuel.hiebeler@gmail.com>
2023-11-07 11:29:24 +0100

    finished sidebar

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -1,16 +1,21 @@
-import { House, Playlist } from "@phosphor-icons/react";
+import { House, List, Playlist, User } from "@phosphor-icons/react";
 import SidebarElement from "./sidebarElement";
+import Image from "next/image";
+import artist from "../../public/icons/artist.png";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [open, setOpen] = useState<boolean>(true);
+
   return (
     <div className="bg-[#212121] h-full pt-3">
-      <div className="">Menu</div>
+      <button onClick={() => setOpen(!open)} className="m-4"><List size={32} /></button>
       <div className="mt-4 flex flex-col space-y-2">
-      </div>
-        <SidebarElement icon={<House size={24} color="#26a269" weight="fill" />} title="Home" />
-        <SidebarElement icon={<Playlist size={24} color="#26a269" weight="fill" />} title="Playlist" />
-        <SidebarElement icon={<House size={24} color="#26a269" weight="fill" />} title="Home" />
-        <SidebarElement icon={<House size={24} color="#26a269" weight="fill" />} title="Home" />
+        <SidebarElement icon={<House size={24} color="#1DB954" weight="fill" />} title="Home" open={open} href="/"/>
+        <SidebarElement icon={<Playlist size={24} color="#1DB954" weight="fill" />} title="Playlists" open={open} href="#"/>
+        <SidebarElement icon={<Image width={24} height={24} src={artist} alt="artist" />} title="Artists" open={open} href="#"/>
+        <SidebarElement icon={<User size={24} color="#1DB954" weight="fill" />} title="Profile" open={open} href="/profile"/>
+        </div>
     </div>
   );
 };
--- components/sidebar/sidebarElement.tsx
+++ components/sidebar/sidebarElement.tsx
@@ -1,16 +1,19 @@
+import Link from "next/link";
 import { ReactNode } from "react";
 
 interface SidebarElementProps {
     icon: ReactNode;
     title: string;
+    open: boolean;
+    href: string;
 }
 
 const SidebarElement = (props: SidebarElementProps) => {
     return (
-        <button className="flex flex-row space-x-4 px-6 py-2 hover:bg-primary hover:bg-opacity-40 border-l-4 border-transparent hover:border-primary duration-100">
-            {props.icon}
-            <p>{props.title}</p>
-        </button>
+        <Link href={props.href} className={`flex flex-row pl-4 ${props.open ? 'pr-8': 'pr-2'} py-2 hover:bg-primary hover:bg-opacity-40 border-l-4 border-transparent hover:border-primary duration-100`}>
+            <div className="mr-4">{props.icon}</div>
+            <p className={`${props.open ? '': 'hidden'}` }>{props.title}</p>
+        </Link>
     )
 }
 
